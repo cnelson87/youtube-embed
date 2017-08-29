34532d16f6efec537ec419aaf6c7f1d8b84c4d3c
Chris Nelson <chris.nelson@wearepop.com>
2017-08-29 14:06:59 -0700

    JS edits

--- _assets/scripts/Config.js
+++ _assets/scripts/Config.js
@@ -1,6 +1,5 @@
-
 /**
-*	application constants
+*	application config
 **/
 
 if (!window.location.origin) {
@@ -11,9 +10,9 @@ var Config = {
 	siteUrl: window.location.origin,
 	isIE9: navigator.userAgent.indexOf('MSIE 9') !== -1,
 	isIE10: navigator.userAgent.indexOf('MSIE 10') !== -1,
-	isIE11: navigator.userAgent.indexOf('MSIE 11') !== -1,
+	isIE11: (navigator.userAgent.indexOf('Windows NT') !== -1 && navigator.userAgent.indexOf('rv:11') !== -1),
 	isAndroid: /(android)/i.test(navigator.userAgent),
 	isIOS: navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false,
 	hasFormValidation: typeof document.createElement('input').checkValidity === 'function',
-	hasTouch: !!('ontouchstart' in window)
+	hasTouch: Boolean('ontouchstart' in window || navigator.maxTouchPoints || navigator.msMaxTouchPoints),
 };
--- _assets/scripts/Main.js
+++ _assets/scripts/Main.js
@@ -1,12 +1,14 @@
 
 var Main = {
 	init: function () {
-		var self = this;
-		// console.log('Main.init');
+		var $html = $('html');
+		if (Config.isIE9) {$html.addClass('ie9');}
+		if (Config.isIE10) {$html.addClass('ie10');}
+		if (Config.isIE11) {$html.addClass('ie11');}
+		if (Config.isAndroid) {$html.addClass('android');}
+		if (Config.isIOS) {$html.addClass('ios');}
 
 		$.event.trigger('Main:initialized');
-
-
 	}
 };
 
